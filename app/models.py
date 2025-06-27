3eb0e251b54c7a22b43e37e4e07cc26972ab1c49
NHelixo <167092094+NHelixo@users.noreply.github.com>
2025-06-27 10:14:20 +0300

    +

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ class Subject(models.Model):
 
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
+    
+    related = models.ForeignKey(Subject, related_name="teacher", on_delete=models.DO_NOTHING)
 
 
 class Class(models.Model):
@@ -15,3 +17,5 @@ class Class(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
+
+    related = models.ForeignKey(Class, related_name="student", on_delete=models.DO_NOTHING)
